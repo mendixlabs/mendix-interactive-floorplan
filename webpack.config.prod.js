e86b636437e4b1d8f2c948c6b11638a9d6def361
JelteMX <jelte.lagendijk@mendix.com>
2021-04-19 14:26:13 +0200

    Possible IE Fix (objectSpread not working) is not working yet

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,6 +7,22 @@ const args = process.argv.slice(2);
 
 const baseConfig = require("./node_modules/@mendix/pluggable-widgets-tools/configs/webpack.config.prod.js");
 
+// TODO: IE FIX (NOT WORKING YET)
+// baseConfig[0].module.rules[1] = {
+//     test: /\.jsx?$/,
+//     use: {
+//         loader: "babel-loader",
+//         options: {
+//             cacheDirectory: true,
+//             presets: ["@babel/preset-env", "@babel/preset-react"],
+//             plugins: [
+//                 ["@babel/plugin-proposal-class-properties", { loose: true }],
+//                 ["@babel/plugin-transform-react-jsx", { pragma: "createElement" }]
+//             ]
+//         }
+//     }
+// }
+
 const TerserPlugin = require("terser-webpack-plugin");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -45,11 +61,13 @@ const customConfig = {
           ]
     },
     plugins: [
-    ],
-    // We add this to further slim down the package
-    resolve: {
-
-    }
+    ]
+    // IE FIX NOT WORKING
+    // resolve: {
+    //     alias: {
+    //         "react-map-interaction": path.resolve(__dirname, "./node_modules/react-map-interaction/src/index")
+    //     }
+    // }
 };
 
 if (args.includes("--analyze")) {
